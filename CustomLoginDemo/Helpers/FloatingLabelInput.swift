cc0c41b6e2be98c17b0f59b0afe5b2905db4c041
xamata <61128570+xamata@users.noreply.github.com>
2022-03-04 02:56:49 -0800

    Added some design improvements

--- CustomLoginDemo/Helpers/FloatingLabelInput.swift
+++ CustomLoginDemo/Helpers/FloatingLabelInput.swift
@@ -64,7 +64,7 @@ class FloatingLabelInput: UITextField {
     func addBottomBorder(){
         let bottomLine = CALayer()
         bottomLine.frame = CGRect(x: 0, y: self.frame.size.height - 1, width: self.frame.size.width, height: 1)
-        bottomLine.backgroundColor = UIColor.gray.cgColor
+        bottomLine.backgroundColor = UIColor.gray.withAlphaComponent(0.2).cgColor
         //  no frame around UITextField, allows for bottom line BorderStyle
         borderStyle = .none
         layer.addSublayer(bottomLine)
